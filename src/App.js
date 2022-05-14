720d3966e74969f0ae3e864a5fbb5583f0f4179a
elvolver <elvolver@mail.ru>
2022-05-14 19:58:57 +0300

    Add reducers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash})
+    }
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
+    return (
+        <div className="App">
+            <div>{cash}</div>
+            <div style={{display: "flex"}}>
+                <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+                <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+            </div>
+            {customers.length > 0 ?
+                <div>
+
+                </div>
+                :
+                <div>
+                    <h1>Клиенты отсутствуют</h1>
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
